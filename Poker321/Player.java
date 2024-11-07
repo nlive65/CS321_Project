b322093fb23b537b2c689b15c0dde0501be91236
giosanchezcortez <gc0059@uah.edu>
2024-11-07 17:03:32 -0600

    Update Player.java
    added fold and check flags, and implemented methods

--- Poker321/Player.java
+++ Poker321/Player.java
@@ -20,11 +20,18 @@ public abstract class Player
     protected int chips; 
     protected CardHand hand; 
     protected int currentBet; 
+    protected boolean hasFolded;
+    protected boolean hasChecked;
 
     // constructor
     public Player(String name, int chips) 
     {
-       
+        this.name = name;
+        this.chips = chips;
+        this.hand = new CardHand(); 
+        this.currentBet = 0;
+        this.hasFolded = false;
+        this.hasChecked = false;
     }
     
     // getters
@@ -48,31 +55,86 @@ public abstract class Player
         return currentBet;
     }
 
+    public boolean hasChecked()
+    {
+        return hasChecked;
+    }
+
+    public boolean hasFolded()
+    {
+        return hasFolded;
+    }
+
     // method to make a move in the game
     public abstract void MakeMove();
 
+    // method to add chips
+    public void addChips(int amount)
+    {
+        if (amount > 0)
+        {
+            this.chips += amount;
+        }
+        else
+        {
+            System.out.println("Invalid chip amount.");
+        }
+    }
+
+    //method to subtract chips for placing bets
+    public void subChips(int amount)
+    {
+        if (amount <= chips && amount > 0)
+        {
+            chips -= amount;        // Deduct bet from player's chips
+            currentBet += amount;   // Set the current bet
+        } 
+        else 
+        {
+            System.out.println("Not enough chips or invalid bet amount.");
+        }
+    }
+    
     // method to place a bet
     public void PlaceBet(int amount) 
     {
-        
+         if (betAmount > 0 && betAmount <= chips) 
+        {
+            this.currentBet = betAmount;
+            subChips(betAmount);  // Deduct the bet amount from chips
+        } 
+        else 
+        {
+            System.out.println("Invalid bet amount!");
+        }
     }
 
     // method to fold
     public void Fold() 
     {
-        
+        this.hasFolded = true; // player folded
+        // message indicating user folded
     }
 
     // method to check
     public void Check() 
     {
-        
+        if (this.currentBet == 0)
+        {
+            this.hasChecked = true; //player checked when no one has bet
+        }
+        else
+        {
+            //message indicating player cannot check
+        }
     }
 
 
     // method to reset the current bet when the rounds are finished
     public void resetBet() 
     {
-        
+        this.currentBet = 0;
+        this.hasFolded = false;
+        this.hasChecked = false;
     }
 }
