5369aa893fa03877d8e07e58e778b3be48ce7026
giosanchezcortez <gc0059@uah.edu>
2024-11-08 02:39:17 -0600

    Update CPU.java

--- Poker321/CPU.java
+++ Poker321/CPU.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package com.mycompany.player;
 
 /**
  *
@@ -12,47 +13,46 @@
 
 public abstract class CPU extends Player 
 {
-    // Current bet amount
-    protected int currentBet;
-
     // constructor
     public CPU(String name, int chips) 
     {
-
+        super(name, chips);
+        this.currentBet = 0;
     }
-
+    
     //method for making a move in the game to be implemented by subclasses    
-    public abstract void MakeMove();
+    public abstract void makeMove();
 
     //method to place a bet
-    public void PlaceBet(int amount) 
+    public void placeBet(int amount) 
     {     
-        
+        if (amount <= chips && amount > 0)
+        {
+            super.currentBet = amount;   // Set the bet amount
+        }
+        else 
+        {
+            System.out.println("Invalid bet amount");
+        }
     }
 
     // method to fold
-    public void Fold() 
+    public void fold() 
     {
-        
+        super.fold();
     }
 
     // method to check
-    public void Check() 
+    public void check() 
     {
-        
-    }
-
-    // method to get the current bet
-    public int GetCurrentBet() 
-    {
-        return currentBet;
+        super.check();
     }
 
     // method to reset the current bet when the rounds are finished
-    public void ResetBet() 
+    public void resetBet() 
     {
-        
+        this.currentBet = 0;
+        this.hasFolded = false;
+        this.hasChecked = false;
     }
 }
-
-
