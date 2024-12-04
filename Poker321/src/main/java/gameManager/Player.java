275636e4b8a9a5d593335f2b3f51e82e10fac430
WesLeeKiv <wlk0006@uah.edu>
2024-12-04 00:49:43 -0600

    Update Player.java

--- Poker321/src/main/java/gameManager/Player.java
+++ Poker321/src/main/java/gameManager/Player.java
@@ -2,11 +2,10 @@ package gameManager;
 
 import card.CardHand;
 
-package gameManager;
-import card.CardHand;
 /**
  *
  * @author Luke
+ * @param takes user name, initial balance, and on what turn they play
  */
 
 // A basic idea of what the Player class will need, will likely be appended when collaborating with CPU classes
@@ -21,6 +20,8 @@ public class Player {
     private boolean isActive;
     private int currentBet;
     private int turn;
+    protected boolean hasFolded;
+    protected boolean hasChecked;
     
     
     // Constructor
@@ -36,55 +37,57 @@ public class Player {
     
     
     // Getters and Setters
+    /** Retrieves player name */
     public String getName() 
     {
         return name;
     }
-    
+    /** Sets player name to input string */
     public void setName(String inputName)
     {
         name = inputName;
     }
-
+    /** Retrieves player turn */
     public int getTurn()
     {
         return turn;
     }
-    
+    /** Sets player turn to input integer */
     public void setTurn(int turn)
     {
         this.turn = turn;
     }
+    /** Retrieves the player hand */
     public CardHand GetCardHand()
     {
         return hand;
     }
-    
+    /** Retrieves player balance */
     public int getBalance() 
     {
         return balance;
     }
-
+    /** Sets player balance to input integer */
     public void setBalance(int balance) 
     {
         this.balance = balance;
     }
-
+    /** Retrieves if the player is active */
     public boolean isActive() 
     {
         return isActive;
     }
-
+    /** Sets the player to inactive/active*/
     public void setActive(boolean active) 
     {
         this.isActive = active;
     }
-
+    /** Retrieves the current bet amount */
     public int getCurrentBet() 
     {
         return currentBet;
     }
-
+    /** Sets the current bet to input integer */
     public void setCurrentBet(int currentBet) 
     {
         this.currentBet = currentBet;
@@ -103,7 +106,36 @@ public class Player {
             throw new IllegalArgumentException("Insufficient balance to place bet");
         }
     }
-    
+   /** Retrieves if the player has checked */
+      public boolean hasChecked()
+    {
+        return hasChecked;
+    }
+   /** Retrieves if the player has folded */
+    public boolean hasFolded()
+    {
+        return hasFolded;
+    }
+    /** Sets the player state to folded */
+    public void fold() 
+    {
+        this.hasFolded = true; 
+        System.out.println(name + " has folded");
+    }
+    /** Allows the player to check */
+    public void check() 
+    {
+        if (this.currentBet == 0)
+        {
+            this.hasChecked = true; 
+            System.out.println(name + " has checked");
+        }
+        else
+        {
+            System.out.println("Bet placed, cannot check");
+        }
+    }
+    /** Increase player balance by input amount */
     public void increaseBalance(int amount)
     {
         balance += amount;
